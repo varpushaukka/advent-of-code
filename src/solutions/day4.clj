e756ecbc912b5fedf0942c5329b8e7096f79c8e8
Henna Kalliokoski <henna.kalliokoski@siili.com>
2019-12-04 20:07:58 +0200

    day4 solutions

---
+++ src/solutions/day4.clj
@@ -0,0 +1,30 @@
+(ns solutions.day4)
+
+
+;part 1
+(defn part1-solutions [] 
+   (for [password (range 171309 643604)
+         :let [lpw (map #(Character/digit % 10) (str password))]
+         :when (every? (fn [[x y]] (>= y x)) (partition 2 1 lpw))
+         :when (some (fn [[x y]] (= x y)) (partition 2 1 lpw))]
+     lpw))
+
+(count (part1-solutions))
+
+;part 2
+(defn no-groups [password]
+  (let [groups (filter (fn [[x y z]] (= x y z)) (partition 3 1 password))]
+    (if-not (empty? groups)
+      (remove #((set (flatten groups)) %) password)
+      password
+      )))
+
+(no-groups '(5 8 8 8 9 9))
+
+(defn part2-solutions []
+  (for [lpw (part1-solutions)
+        :let [pw (no-groups lpw)]
+        :when (some (fn [[x y]] (= x y)) (partition 2 1 pw))]
+    pw))
+
+(count (part2-solutions))
