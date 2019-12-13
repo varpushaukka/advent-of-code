f81af86640916f26c169f571848222ee1a91d21d
Henna Kalliokoski <henna.kalliokoski@siili.com>
2019-12-13 11:22:46 +0200

    use reduce in day8

--- src/solutions/day8.clj
+++ src/solutions/day8.clj
@@ -7,15 +7,8 @@
        (partition (* 25 6))))
 
 (defn find-fewest-0-digits [l-list]
-  (let [zfreq (fn [x] (if (contains? (set x) 0) 
-                        (get (frequencies x) 0)
-                        0))]
-    (loop [result (first l-list) l (rest l-list)]
-      (cond
-        (empty? l) result
-        (< (zfreq result) (zfreq (first l))) (recur result (rest l))
-        :else (recur (first l) (rest l))
-        ))))
+  (let [zfreq (fn [x y] (if (< (get (frequencies x) 0) (get (frequencies y) 0)) x y))]
+    (reduce zfreq l-list)))
 
 (defn part-1 []
   (let [freq (frequencies (find-fewest-0-digits layers))
