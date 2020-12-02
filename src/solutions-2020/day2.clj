0e2d914cf1c0a7e78731cd06651da0b51becada9
Henna Kalliokoski <henna.kalliokoski@siili.com>
2020-12-03 00:33:10 +0200

    day2

---
+++ src/solutions-2020/day2.clj
@@ -0,0 +1,17 @@
+(def input
+  (for [policy (-> "src/solutions-2020/data/day2input.txt" slurp (clojure.string/split #"\n"))]
+    (re-seq #"\w+" policy)))
+
+(defn valid-passwords [pw-policies]
+  (filter (fn [[min max target password]]
+            (<= (read-string min)
+                (get (frequencies password) (first target) -1)
+                (read-string max))) pw-policies))
+
+(count (valid-passwords input))
+
+(defn valid-passwords-2 [pw-policies]
+  (filter (every-pred (fn [[i1 i2 target password]] (not (every? #{(first target)} [(get password (dec (read-string i1))) (get password (dec (read-string i2)))])))
+                         (fn [[i1 i2 target password]] (some #{(first target)} [(get password (dec (read-string i1))) (get password (dec (read-string i2)))]))) pw-policies))
+
+(count (valid-passwords-2 input))
