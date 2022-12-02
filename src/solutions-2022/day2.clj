d8ac47e7e3e61e9043d01e2389e8d85af6d3fcec
Henna Kalliokoski <henna.kalliokoski@reaktor.com>
2022-12-02 15:41:49 +0200

    cleanup day2

--- src/solutions-2022/day2.clj
+++ src/solutions-2022/day2.clj
@@ -5,27 +5,27 @@
   (for [game (-> "src/solutions-2022/data/day2input.txt" slurp (str/split #"\n"))]
     (re-seq #"\w+" game)))
 
-(defn parse-hand [hand parse-map]
+(defn parse-hand [hand]
   (map
-   (comp #(read-string %) #(str/replace % #"A|B|C|X|Y|Z" parse-map))
+   (comp #(read-string %) #(str/replace % #"A|B|C|X|Y|Z" {"A" "1" "B" "2" "C" "3" "X" "1" "Y" "2" "Z" "3"}))
    hand))
 
 ; part 1
 (defn calc-points [[a b]]
-     (cond 
-       (= (mod a 3) (dec b)) (+ 6 b)
-       (= a b) (+ 3 b)
-       :else b
-       ))
+  (cond
+    (= (mod a 3) (dec b)) (+ 6 b)
+    (= a b) (+ 3 b)
+    :else b))
 
-(reduce + (map (comp calc-points #(parse-hand % {"A" "1" "B" "2" "C" "3" "X" "1" "Y" "2" "Z" "3"})) input))
+(reduce + (map (comp calc-points parse-hand) input))
 
 ; part 2
 (defn new-strategy [[a b]]
-    (case b
-      0 (calc-points [a (get {1 3 2 1 3 2} a)])
-      3 (calc-points [a a])
-      6 (calc-points [a (get {1 2 2 3 3 1} a)])
-    ))
+  (case b
+    1 (calc-points [a (get {1 3 2 1 3 2} a)])
+    2 (calc-points [a a])
+    3 (calc-points [a (get {1 2 2 3 3 1} a)])))
 
-(reduce + (map (comp new-strategy #(parse-hand % {"A" "1" "B" "2" "C" "3" "X" "0" "Y" "3" "Z" "6"})) input))
+(reduce + (map
+           (comp new-strategy parse-hand)
+           input))
